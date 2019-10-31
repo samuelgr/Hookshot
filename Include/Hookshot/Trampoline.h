a16ad5588af84e8baab9fe6ec36e541c5ed93618
Samuel Grossman <samuelgr@users.noreply.github.com>
2019-10-30 22:26:44 -0700

    Added a skeleton implementation for trampolines.

---
+++ Include/Hookshot/Trampoline.h
@@ -0,0 +1,100 @@
+/******************************************************************************
+ * Hookshot
+ *   General-purpose library for hooking API calls in spawned processes.
+ ******************************************************************************
+ * Authored by Samuel Grossman
+ * Copyright (c) 2019
+ **************************************************************************//**
+ * @file Trampoline.h
+ *   Data structure declaration for individual trampolines.
+ *****************************************************************************/
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+
+namespace Hookshot
+{
+    /// Generates and holds trampoline code used to invoke the original functionality of a hooked function.
+    class Trampoline
+    {
+    private:
+        // -------- CONSTANTS ---------------------------------------------- //
+
+        /// Size of the trampoline, in bytes.
+        /// Must be divisible by 16.
+        static constexpr size_t kTrampolineSizeBytes = 32;
+
+        /// Initial value to be placed at the start of the trampoline code to indicate it is uninitialized.
+        /// Consists of a "ud2" instruction followed by a two-byte "nop" instruction.
+        /// The "ud2" ensures any attempt to execute will generate an error.
+        static constexpr uint32_t kCodeUninitialized = 0x90660b0f;
+
+        /// Value to be placed at the start of the trampoline code to indicate an attempt to set it failed.
+        /// Consists of two back-to-back "ud2" instructions.
+        static constexpr uint32_t kCodeFailedSet = 0x0b0f0b0f;
+
+
+        // -------- TYPE DEFINITIONS --------------------------------------- //
+
+        /// Raw trampoline code type.
+        /// Large enough to hold the entire code region, and allows different access granularities.
+        union UTrampolineCode
+        {
+            uint8_t byte[kTrampolineSizeBytes / sizeof(uint8_t)];       ///< Byte-level access
+            uint16_t word[kTrampolineSizeBytes / sizeof(uint16_t)];     ///< Word-level access
+            uint32_t dword[kTrampolineSizeBytes / sizeof(uint32_t)];    ///< Doubleword-level access
+            uint64_t qword[kTrampolineSizeBytes / sizeof(uint64_t)];    ///< Quadword-level access
+            size_t ptr[kTrampolineSizeBytes / sizeof(size_t)];          ///< Pointer-sized access
+        };
+
+
+        // -------- INSTANCE VARIABLES ------------------------------------- //
+
+        /// Holds the trampoline code itself.
+        UTrampolineCode code;
+
+
+    public:
+        // -------- CONSTRUCTION AND DESTRUCTION --------------------------- //
+
+        /// Default constructor.
+        inline Trampoline(void)
+        {
+            Reset();
+        }
+
+
+        // -------- INSTANCE METHODS --------------------------------------- //
+
+        /// Specifies if this trampoline is initialized (i.e. an attempt to set the trampoline has been made).
+        /// @return `true` if so, `false` otherwise.
+        inline bool IsInitialized(void) const
+        {
+            return (kCodeUninitialized != code.dword[0]);
+        }
+
+        /// Specifies if this trampoline is successfully set.
+        /// @return `true` if so, `false` otherwise.
+        inline bool IsSet(void) const
+        {
+            return (IsInitialized() && (kCodeFailedSet != code.dword[0]));
+        }
+
+        /// Resets this trampoline to its uninitialized state.
+        inline void Reset(void)
+        {
+            code.dword[0] = kCodeUninitialized;
+        }
+
+        /// Sets the trampoline so it effectively jumps to the specified address when executed.
+        /// Reads the first instruction at the specified address and copies it into the trampoline.
+        /// Appends a "jmp" instruction that targets the second instruction at the specified address.
+        /// Does not modify the function at the specified address.
+        /// @param [in] target Instruction address that this trampoline should target.
+        /// @return `true` if successful, `false` otherwise.
+        bool SetTarget(void* target);
+    };
+}
---
+++ Source/Trampoline.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+ * Hookshot
+ *   General-purpose library for hooking API calls in spawned processes.
+ ******************************************************************************
+ * Authored by Samuel Grossman
+ * Copyright (c) 2019
+ **************************************************************************//**
+ * @file Trampoline.cpp
+ *   Implementation of some functionality for individual trampolines.
+ *****************************************************************************/
+
+#include "Trampoline.h"
+
+#include <cstddef>
+#include <cstdint>
+
+
+namespace Hookshot
+{
+    // -------- INSTANCE METHODS --------------------------------------- //
+    // See "Hookshot.h" for documentation.
+
+    bool Trampoline::SetTarget(void* target)
+    {
+        // TODO: implement this method for real.
+        code.dword[0] = kCodeFailedSet;
+        return false;
+    }
+}
