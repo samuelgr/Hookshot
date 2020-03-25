98533b3943ba69966f2ddd6c1d8db9b668f768bf
Samuel Grossman <samuelgr@users.noreply.github.com>
2020-03-25 00:51:07 -0700

    Added some tests to capture a Hookshot bug related to not compensating for the encoder removing useless REX.W prefixes from instructions with position-dependent memory accesses.

---
+++ Source/Test/Case/HookSetSuccess/JumpAbsolutePositionRelative.asm
@@ -0,0 +1,51 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Hookshot
+;   General-purpose library for injecting DLLs and hooking function calls.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Authored by Samuel Grossman
+; Copyright (c) 2019-2020
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+INCLUDE TestDefinitions.inc
+
+
+; When run in 64-bit mode, this test exercises RIP-relative addressing for loading an absolute jump target from memory.
+; Hook function does nothing special for this test.
+
+
+_TEXT                                       SEGMENT
+
+
+BEGIN_HOOKSHOT_TEST_FUNCTION                JumpAbsolutePositionRelative_Original
+    jmp SIZE_T PTR [$jumpTargetAbsolute]
+    ud2
+    ud2
+
+$return:
+    mov sax, scx
+    ret
+    
+    REPEAT 25
+        BYTE 255
+    ENDM
+
+$jumpTargetAbsolute:
+    SIZE_T $return
+
+    REPEAT 35
+        BYTE 255
+    ENDM
+END_HOOKSHOT_TEST_FUNCTION                  JumpAbsolutePositionRelative_Original
+
+
+BEGIN_HOOKSHOT_TEST_FUNCTION                JumpAbsolutePositionRelative_Hook
+    mov sax, scx
+    shl sax, 1
+    ret
+END_HOOKSHOT_TEST_FUNCTION                  JumpAbsolutePositionRelative_Hook
+
+
+_TEXT                                       ENDS
+
+
+END
---
+++ Source/Test/Case/HookSetSuccess/JumpAbsolutePositionRelativeUselessPrefix.asm
@@ -0,0 +1,56 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Hookshot
+;   General-purpose library for injecting DLLs and hooking function calls.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Authored by Samuel Grossman
+; Copyright (c) 2019-2020
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+INCLUDE TestDefinitions.inc
+
+
+; This test is only valid in 64-bit mode; as written, it will crash in 32-bit mode.
+; It exercises RIP-relative addressing for loading an absolute jump target from memory and adds a useless REX.W to the jump instruction, which the encoder might remove.
+; If the encoder removes the prefix, without further compensation on the part of Hookshot the transplanted jump displacement will be wrong (off by one byte).
+; Should the transplanted displacement be off, the jump target will also be off (in this case filled with 1s), likely leading to a program crash.
+; Hook function does nothing special for this test.
+
+
+_TEXT                                       SEGMENT
+
+
+BEGIN_HOOKSHOT_TEST_FUNCTION                JumpAbsolutePositionRelativeUselessPrefix_Original
+IFDEF HOOKSHOT64
+    rexw jmp SIZE_T PTR [$jumpTargetAbsolute]
+ENDIF
+    ud2
+    ud2
+
+$return:
+    mov sax, scx
+    ret
+    
+    REPEAT 25
+        BYTE 255
+    ENDM
+
+$jumpTargetAbsolute:
+    SIZE_T $return
+
+    REPEAT 35
+        BYTE 255
+    ENDM
+END_HOOKSHOT_TEST_FUNCTION                  JumpAbsolutePositionRelativeUselessPrefix_Original
+
+
+BEGIN_HOOKSHOT_TEST_FUNCTION                JumpAbsolutePositionRelativeUselessPrefix_Hook
+    mov sax, scx
+    shl sax, 1
+    ret
+END_HOOKSHOT_TEST_FUNCTION                  JumpAbsolutePositionRelativeUselessPrefix_Hook
+
+
+_TEXT                                       ENDS
+
+
+END
