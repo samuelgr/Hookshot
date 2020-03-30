c3f2fc59c094e33c8003a094d30d8f7ca6c308d4
Samuel Grossman <samuelgr@users.noreply.github.com>
2020-03-29 18:22:50 -0700

    Added a test program to the examples, which will be used to demonstrate Hookshot's ability to modify a program's behavior.

---
+++ Examples/Source/TestProgram/TestProgram.cpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Hookshot
+ *   General-purpose library for hooking API calls in spawned processes.
+ ******************************************************************************
+ * Authored by Samuel Grossman
+ * Copyright (c) 2019
+ **************************************************************************//**
+ * @file TestProgram.cpp
+ *   Test program used to demonstrate the example hook modules.
+ *****************************************************************************/
+
+#include <windows.h>
+
+ 
+// -------- INTERNAL VARIABLES --------------------------------------------- //
+
+/// Message box text displayed by the test program.
+static constexpr wchar_t kMessageBoxText[] = L"This is a test program.";
+
+/// Title of the message box displayed by the test program.
+static constexpr wchar_t kMessageBoxTitle[] = L"TestProgram";
+
+
+// -------- ENTRY POINT ---------------------------------------------------- //
+
+/// Program entry point.
+/// @param [in] hInstance Instance handle for this executable.
+/// @param [in] hPrevInstance Unused, always `nullptr`.
+/// @param [in] lpCmdLine Command-line arguments specified after the executable name.
+/// @param [in] nCommandShow Flag that specifies how the main application window should be shown. Not applicable to this executable.
+/// @return `TRUE` if this function successfully initialized or uninitialized this library, `FALSE` otherwise.
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
+{
+    // This test program simply generates a message box and then exits.
+    // Hook module examples use Hookshot to hook `MessageBoxW` and modify the message box that is displayed.
+    // Examine the code for each one to see what modification is performed.
+    // Unmodified, the message box has a single OK button and no icon.  It displays the contents of the variables defined at the top of this file.
+
+    MessageBoxW(nullptr, kMessageBoxText, kMessageBoxTitle, MB_OK);
+    return 0;
+}
