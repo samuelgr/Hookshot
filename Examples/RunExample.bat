84ecfa1c36f33c70a9b1a14ce2193ab47d10b82c
Samuel Grossman <samuelgr@users.noreply.github.com>
2020-03-29 22:14:00 -0700

    Improved the experience of running examples from Visual Studio.  Added some error checks to the script, which are displayed to users if they fail.

--- Examples/RunExample.bat
+++ Examples/RunExample.bat
@@ -1,13 +1,27 @@
 @echo off
+rem +--------------------------------------------------------------------------
+rem | Hookshot
+rem |   General-purpose library for injecting DLLs and hooking function calls.
+rem +--------------------------------------------------------------------------
+rem | Authored by Samuel Grossman
+rem | Copyright (c) 2019-2020
+rem +--------------------------------------------------------------------------
+rem | RunExample.bat
+rem |   Script for running hook module examples.  Invoked by Visual Studio
+rem |   when attempting to run a hook module example project.
+rem +--------------------------------------------------------------------------
 
-rem Read command-line arguments.
 set hookshot_exe=%1
 set test_program_exe=%2
 set out_dir=%3
 set project_name=%4
 
-rem Create the Hookshot.ini configuration file.
 echo HookModule = %project_name% > %out_dir%Hookshot.ini
 
-rem Run the Hookshot executable and point it to the test program.
-%hookshot_exe% %test_program_exe%
+if exist %hookshot_exe% (
+    %hookshot_exe% %test_program_exe%
+) else (
+    echo %hookshot_exe%
+    echo File does not exist!  Build Hookshot and try again.
+    pause
+)
