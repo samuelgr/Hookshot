d405d99af5fc7745563d966a6d38f2ff3b596158
Samuel Grossman <samuelgr@users.noreply.github.com>
2025-01-01 10:50:08 -0800

    Move documentation over to the wiki.

--- PackageRelease.bat
+++ PackageRelease.bat
@@ -7,7 +7,7 @@ set project_platforms=Win32 x64
 set project_has_sdk=yes
 set project_has_third_party_license=yes
 
-set files_release=LICENSE *.md
+set files_release=LICENSE README.md
 
 set files_release_build_Win32=Hookshot.32.exe Hookshot.32.dll HookshotLauncher.32.exe
 set files_release_build_x64=Hookshot.64.exe Hookshot.64.dll HookshotLauncher.64.exe
